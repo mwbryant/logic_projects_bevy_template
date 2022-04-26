9e32460906d813d21cc8e18cd61bf0f4af2b2b1e
Matthew <logicprojectsforfpgas@gmail.com>
2022-04-26 13:22:56 -0600

    Init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bevy Tutorial Template
+
+A simple template to base [Bevy](https://bevyengine.org/) tutorials from. Creates a window, adds common dependancies, and sets up debugging utils.
+
+# Usage
+
+```
+cargo generate --git https://github.com/mwbryant/logic_projects_bevy_template
+```
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+#![allow(clippy::redundant_field_names)]
+#![allow(clippy::type_complexity)]
+#![allow(clippy::too_many_arguments)]
+
+use bevy::{prelude::*, render::camera::ScalingMode, window::PresentMode};
+use bevy_inspector_egui::{WorldInspectorParams, WorldInspectorPlugin};
+
+pub const CLEAR: Color = Color::rgb(0.3, 0.3, 0.3);
+pub const HEIGHT: f32 = 900.0;
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: HEIGHT * RESOLUTION,
+            height: HEIGHT,
+            title: "Bevy Template".to_string(),
+            present_mode: PresentMode::Fifo,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .insert_resource(WorldInspectorParams {
+            enabled: false,
+            ..Default::default()
+        })
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_startup_system(spawn_camera)
+        .add_system(toggle_inspector)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
+
+    camera.orthographic_projection.right = HEIGHT / 2.0 * RESOLUTION;
+    camera.orthographic_projection.left = -HEIGHT / 2.0 * RESOLUTION;
+
+    camera.orthographic_projection.top = HEIGHT / 2.0;
+    camera.orthographic_projection.bottom = -HEIGHT / 2.0;
+
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
+}
+
+fn toggle_inspector(
+    input: ResMut<Input<KeyCode>>,
+    mut window_params: ResMut<WorldInspectorParams>,
+) {
+    if input.just_pressed(KeyCode::Grave) {
+        window_params.enabled = !window_params.enabled
+    }
+}
+
+#[allow(dead_code)]
+fn slow_down() {
+    std::thread::sleep(std::time::Duration::from_secs_f32(1.000));
+}
